54d42dad91386974f5f6377e4c98333d7ee8e08e
MarcusBlachek <marcus_blachek@hotmail.com>
2021-08-23 18:32:52 -0300

    Botão Cadastrar incluído no canto superior direito, com sua view de formulário para cadastro pronta. No Controller, incluída a classe ContaController com suas Actions Cadastrar para chamar a View e POST do formulário já implantadas.

---
+++ Controllers/ContaController.cs
@@ -0,0 +1,21 @@
+﻿using GerenciamentoDeIdentidades.ViewModels;
+using System.Web.Mvc;
+
+namespace GerenciamentoDeIdentidades.Controllers
+{
+    public class ContaController : Controller
+    {
+        //chamada da view Cadastrar
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        //método POST do cadastro na View. Submeter as informações inseridas no formulário
+        [HttpPost]
+        public ActionResult Cadastrar(ContaCadastrarViewModel modelo)
+        {
+            return View();
+        }
+    }
+}
---
+++ ViewModels/ContaCadastrarViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GerenciamentoDeIdentidades.ViewModels
+{
+    //classe com os modelos e configurações PARA o formulário.
+    public class ContaCadastrarViewModel
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [Display(Name ="Nome Completo")]
+        public string Nomecompleto { get; set; }
+        [Required]
+        [EmailAddress]
+        public string  Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+    }
+}
